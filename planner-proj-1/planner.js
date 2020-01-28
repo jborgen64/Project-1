92b1731ad800e4c4225f492f75c78cd63c491ee6
son loku <jacobrborgen@gmail.com>
2020-01-28 15:58:55 -0600

    added uniformity to all of the month divs so they display consistently

--- planner-proj-1/planner.js
+++ planner-proj-1/planner.js
@@ -1,17 +1,107 @@
 //display month divs as buttons are clicked
 
+//jan display
+
 $("#janClick").on("click", function() {
     $(".months").hide(500);
     $("#janDisplay").show(500);
 });
 
+//feb display
+
+$("#febClick").on("click", function() {
+    $(".months").hide(500);
+    $("#febDisplay").show(500);
+});
+
+//mar display
+
+$("#marClick").on("click", function() {
+    $(".months").hide(500);
+    $("#marDisplay").show(500);
+});
+
+//apr display
+
+$("#aprClick").on("click", function() {
+    $(".months").hide(500);
+    $("#aprDisplay").show(500);
+});
+
+//may display
+
+$("#mayClick").on("click", function() {
+    $(".months").hide(500);
+    $("#mayDisplay").show(500);
+});
+
+//jun display
+
+$("#junClick").on("click", function() {
+    $(".months").hide(500);
+    $("#junDisplay").show(500);
+});
+
+//jul display
+
+$("#julClick").on("click", function() {
+    $(".months").hide(500);
+    $("#julDisplay").show(500);
+});
+
+//aug display
+
+$("#augClick").on("click", function() {
+    $(".months").hide(500);
+    $("#augDisplay").show(500);
+});
+
+//sep display
+
+$("#sepClick").on("click", function() {
+    $(".months").hide(500);
+    $("#sepDisplay").show(500);
+});
+
+//oct display
+
+$("#octClick").on("click", function() {
+    $(".months").hide(500);
+    $("#octDisplay").show(500);
+});
+
+//nov display
+
+$("#novClick").on("click", function() {
+    $(".months").hide(500);
+    $("#novDisplay").show(500);
+});
+
+//dec display
+
+$("#decClick").on("click", function() {
+    $(".months").hide(500);
+    $("#decDisplay").show(500);
+});
+
 //append typed books to current div
 
 //Jan Books to be saved
-
-$("#bookSubmit").on("click", function() {
-    var book = $("#bookInput").val();
-    var bookSaved = $("<option>").append(book);
-    $("#janBookHolder").prepend(bookSaved);
+var janBookSaved;
+$("#janBookSubmit").on("click", function() {
+    var book = $("#janBookInput").val();
+    var janBookSaved = $("<option>").append(book);
+    $("#janBookHolder").prepend(janBookSaved);
     $("#janBookHolder").show(500);
+});
+
+
+
+//Feb Books to be saved
+
+$("#febBookSubmit").on("click", function() {
+    var book = $("#febBookInput").val();
+    var bookSaved = $("<option>").append(book);
+    $("#febBookHolder").prepend(bookSaved);
+    $("#febBookHolder").show(500);
 });
