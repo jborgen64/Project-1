303140e8adfc5201b26be019b063772032b84a2d
son loku <jacobrborgen@gmail.com>
2020-01-28 14:57:29 -0600

    added an onclick function that prepends the book to an options list that will later submit to local storage

--- planner-proj-1/planner.js
+++ planner-proj-1/planner.js
@@ -1,4 +1,17 @@
+//display month divs as buttons are clicked
+
 $("#janClick").on("click", function() {
     $(".months").hide(500);
     $("#janDisplay").show(500);
+});
+
+//append typed books to current div
+
+//Jan Books to be saved
+
+$("#bookSubmit").on("click", function() {
+    var book = $("#bookInput").val();
+    var bookSaved = $("<option>").append(book);
+    $("#janBookHolder").prepend(bookSaved);
+    $("#janBookHolder").show(500);
 });
