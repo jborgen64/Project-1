f2f2a8b3c37c473cef7d248a73aa81d8f443e97b
Lawrence Adrian De Belen <ltdebelen@gmail.com>
2020-01-29 19:59:50 -0600

    Adding final version of website. Adding Asset Files

--- README.md
+++ README.md
@@ -1,7 +1,45 @@
-# BookReso
-An app that will help you find and track books throughout the year. 
-Will have a slick UI that will utilize smooth scroll for great user experience. 
-Will implement Google Books API to help find new books to read and the app will provide recommendations.
-Will utilize Map Box API to help find nearest book stores.
-Will have a built in planner for helping track books each month.
-Bulma Lux Theme for great presentation.
+# Project 1: Book Reso
+
+## Description
+
+A simple web application that helps users achieve their New Year's Resolution by giving them book suggestions categorized by top New Year's Resolution.
+
+## Main Features and Functions
+
+- Implemented Bulma with Lux theme for great presentation.
+
+* Applied slick carousel and smooth scrolling for better UI transitions.
+
+- Suggest and filter books based on the most common new year's resolution.
+
+* Has a built in planner for helping in tracking book readings per month.
+
+- Ability to search for bookstores powered by MapBox API.
+
+## Other Criteria/Features
+
+- Must use at least two server-side APIs
+
+* Must use a CSS framework other than Bootstrap
+
+- Must be interactive (i.e: accept and respond to user input)
+
+* Use at least one new third-party API
+
+- Must have a polished UI
+
+* Must meet good quality coding standards
+
+- Does not use alerts, confirms or prompts (look into modals)
+
+* Must be deployed to GitHub Pages
+
+## Technologies Used
+
+- Must use at least two server-side APIs: (Google Books API, MapBox API)
+- Must use a CSS framework other than Bootstrap: (Bulma: Lux Theme)
+- Use at least one new third-party API (Smooth Scroll, Slick Carousel, Isotopes)
+
+## GitHub Hosted Link
+
+https://jborgen64.github.io/Project-1/
---
+++ assets/js/books.js
@@ -0,0 +1,102 @@
+const books = [
+  {
+    title: "Grit",
+    author: "Angela Duckworth",
+    category: "mindset"
+  },
+  {
+    title: "The Power of Now",
+    author: "Eckhart Tolle",
+    category: "mindset"
+  },
+  {
+    title: "Atomic Habits",
+    author: "James Clear",
+    category: "mindset"
+  },
+  {
+    title: "Becoming",
+    author: "Michelle Obama",
+    category: "mindset"
+  },
+  {
+    title: "Rich Dad Poor Dad",
+    author: "Robert Kiyosaki",
+    category: "finance"
+  },
+  {
+    title: "Think and Grow Rich",
+    author: "Napoleon Hill",
+    category: "finance"
+  },
+  {
+    title: "Money A to Z",
+    author: "Scott Alan Turner",
+    category: "finance"
+  },
+  {
+    title: "The Millionaire Next Door",
+    author: "Thomas Stanley",
+    category: "finance"
+  },
+  {
+    title: "Boundless",
+    author: "Ben Greenfield",
+    category: "fitness"
+  },
+  {
+    title: "The Power of Habit",
+    author: "Charles Duhigg",
+    category: "fitness"
+  },
+  {
+    title: "Spark",
+    author: "John Ratey",
+    category: "fitness"
+  },
+  {
+    title: "Sleep Smarter",
+    author: "Shawn Stevenson",
+    category: "fitness"
+  },
+  {
+    title: "The Rings of Saturn",
+    author: "W.G. Sebald",
+    category: "travel"
+  },
+  {
+    title: "Vagabonding",
+    author: "Rolf Potts",
+    category: "travel"
+  },
+  {
+    title: "Wild",
+    author: "Cheryl Strayed",
+    category: "travel"
+  },
+  {
+    title: "The Art of Travel",
+    author: "Alain De Botton",
+    category: "travel"
+  },
+  {
+    title: "Make it stick",
+    author: "Peter Brown",
+    category: "learning"
+  },
+  {
+    title: "A Mind for Numbers",
+    author: "Barbara Oakley",
+    category: "learning"
+  },
+  {
+    title: "The Magic of Math",
+    author: "Arthur Benjamin",
+    category: "learning"
+  },
+  {
+    title: "Clean Coe",
+    author: "Robert Cecil Martin",
+    category: "learning"
+  }
+];
